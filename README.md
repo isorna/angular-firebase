c67885b98a2c78d03c7b88bf7af9424cf043bf9e
Alvaro Isorna <alvaro.isorna@gmail.com>
2014-12-19 12:57:49 +0000

    AngularFire Demo

--- README.md
+++ README.md
@@ -13,3 +13,20 @@ Web app used for the meetup @fictiziaformacion.
  * `cd` into your website directory and run `firebase init`.
  * Then deploy your website with `firebase deploy`.
 4. Visit your app online: [angularfire-meetup.firebaseapp.com](https://angularfire-meetup.firebaseapp.com/).
+
+## Firebase
+
+* Hosting for static apps, and includes deployment rollbacks.
+* Document database hosting, with JSON import/export.
+* Declarative security rules implementation on server via JSON.
+* Built-in simulator and analytics.
+* User Login & Authentication.
+ * Register the corresponding app (Github, twitter, ...), then activate it from the dashboard.
+ * Use firebase api for authentication: [login with github](https://www.firebase.com/docs/web/guide/login/github.html).
+
+## Links
+
+* [AngularFire documentation](https://www.firebase.com/docs/web/libraries/angular/).
+* [User authentication with firebase](https://www.firebase.com/docs/web/guide/user-auth.html).
+* [Security & rules guide](https://www.firebase.com/docs/security/guide/securing-data.html).
+* [Angular Material Design project on github](https://github.com/angular/material).
--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,110 @@
 /* app.js */
-/*globals: angular */
+/*globals: angular, Firebase */
 
-angular.module('angularFireDemo', []);
+var app = angular.module('angularFireDemo', ['firebase', 'ngMaterial'])
+    .factory('Auth', ['$firebaseAuth', function($firebaseAuth) {
+        var oRef = new Firebase('https://angularfire-meetup.firebaseio.com/');
+        
+        return $firebaseAuth(oRef);
+    }])
+    .factory('Messenger', ['$firebase', function($firebase) {
+        var oRef = new Firebase('https://angularfire-meetup.firebaseio.com/messages/').orderByChild('timestamp').limitToLast(50);
+        
+        return $firebase(oRef).$asArray();
+    }])
+    .filter('reverse', function() {
+        return function(paItems) {
+            return paItems.slice().reverse();
+        };
+    })
+    .controller('MessagesCtrl', ['$scope', 'Auth', '$mdDialog', 'Messenger', function ($scope, Auth, $mdDialog, Messenger) {
+        $scope.userInfo = {
+            authenticated: false
+        };
+        
+        $scope.setAuth = function (pbValue) {
+            $scope.userInfo.authenticated = pbValue;
+        };
+        
+        Auth.$onAuth(function (poAuthData) {
+            if (poAuthData) {
+                $scope.setAuth(true);
+                console.log('Authenticated successfully with payload:', poAuthData);
+            } else {
+                $scope.setAuth(false);
+                console.log('User is logged off');
+            }
+            
+            $scope.user = poAuthData;
+            
+            if (!$scope.$$phase) {
+                $scope.$apply();
+            }
+        });
+        
+        $scope.logIn = function (poEvent) {
+            Auth.$authWithOAuthPopup('github', function(poError, poAuthData) {
+                if (poError) {
+                    $scope.setAuth(false);
+                    console.log('Login Failed!', poError);
+                } else {
+                    $scope.setAuth(true);
+                    console.log('Authenticated successfully with payload (login):', poAuthData);
+                }
+                
+                if (!$scope.$$phase) {
+                    $scope.$apply();
+                }
+            }, {
+                remember: 'sessionOnly'
+            });
+        };
+        
+        $scope.logOut = function (poEvent) {
+            Auth.$unauth();
+            $scope.setAuth(false);
+            
+            if (!$scope.$$phase) {
+                $scope.$apply();
+            }
+            
+            console.log('User has logged off');
+        };
+        
+        $scope.post = function (poEvent) {
+            $mdDialog.show({
+                controller: DialogController,
+                templateUrl: 'dialog.tmpl.html',
+                targetEvent: poEvent,
+            })
+            .then(function(pcMessage) {
+                var dTimestamp = new Date();
+                
+                console.log('You said the information was "' + pcMessage + '".');
+                Messenger.$add({
+                    text: pcMessage,
+                    who: $scope.user.github.displayName,
+                    face: $scope.user.github.cachedUserProfile.avatar_url,
+                    url: $scope.user.github.cachedUserProfile.html_url,
+                    timestamp: dTimestamp.getTime()
+                })
+            }, function() {
+                console.log('You cancelled the dialog.');
+            });
+        };
+        
+        function DialogController($scope, $mdDialog) {
+            $scope.hide = function() {
+                $mdDialog.hide();
+            };
+            $scope.cancel = function() {
+                $mdDialog.cancel();
+            };
+            $scope.send = function(pcMessage) {
+                $mdDialog.hide(pcMessage);
+            };
+        }
+        
+        $scope.messages = Messenger;
+    }])
+;
